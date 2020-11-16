ba209b0b218a4e11be9c53feec5e9719e5ef96ae
ovsiannikov <ovsiannikov@ucu.edu.ua>
2020-11-16 16:42:57 +0200

    minor changes

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,151 +1,144 @@
 package ua.edu.ucu.tempseries;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
 
 public class TemperatureSeriesAnalysis {
-    static final int MIN_TEMP = -273;
-    static final double DELTA = 1e-6;
 
-    private double[] temperatures;
+    private static final double MIN_TEMP = -273.0;
+
+    private double[] temperatureSeries;
+    private int length;
     private int capacity;
-    private int temperaturesNum;
 
-    TemperatureSeriesAnalysis() {
-        temperatures = new double[1];
+    public TemperatureSeriesAnalysis() {
+        temperatureSeries = new double[]{};
+        this.length = 0;
         capacity = 1;
-        temperaturesNum = 0;
     }
 
-    TemperatureSeriesAnalysis(double[] temperatureSeries) {
-        this();
-        for (double temperature : temperatureSeries) {
-            if (temperature < MIN_TEMP) {
+    public TemperatureSeriesAnalysis(double[] tempSeries) {
+        this.temperatureSeries = new double[tempSeries.length];
+        System.arraycopy(tempSeries, 0,
+                this.temperatureSeries, 0, tempSeries.length);
+        this.length = temperatureSeries.length;
+        capacity = temperatureSeries.length;
+        for (double item : this.temperatureSeries) {
+            if (item < MIN_TEMP) {
                 throw new InputMismatchException();
             }
         }
-        temperatures = temperatureSeries;
-        capacity = temperatures.length;
-        temperaturesNum = capacity;
+
     }
 
-    double average() {
-        if (temperaturesNum == 0) {
+    public void isEmpty() {
+        if (this.length == 0) {
             throw new IllegalArgumentException();
         }
-        double sum = 0;
-        for (int i = 0; i < temperaturesNum; i++) {
-            sum += temperatures[i];
-        }
-        return sum / temperaturesNum;
     }
 
-
-    double deviation() {
-        double mean = average();
-        double quadraticSum = 0;
-        for (int i = 0; i < temperaturesNum; i++) {
-            quadraticSum += Math.abs(temperatures[i] - mean)
-                    * Math.abs(temperatures[i] - mean);
+    public double average() {
+        isEmpty();
+        double averageValue = 0;
+        for (double item : this.temperatureSeries) {
+            averageValue += item;
         }
-        return quadraticSum / temperaturesNum;
+        return averageValue / this.length;
     }
 
-    double min() {
-        if (temperaturesNum == 0) {
-            throw new IllegalArgumentException();
+    public double deviation() throws IllegalArgumentException {
+        isEmpty();
+        double minVal = average();
+        double standartDev = 0;
+        for (double item : this.temperatureSeries) {
+            standartDev += (item - minVal) * (item - minVal);
         }
-        double m = temperatures[0];
-        for (int i = 0; i < temperaturesNum; i++) {
-            m = Math.min(m, temperatures[i]);
-        }
-        return m;
+        standartDev /= this.length;
+        return Math.sqrt(standartDev);
     }
 
-    double max() {
-        if (temperaturesNum == 0) {
-            throw new IllegalArgumentException();
-        }
-        double m = temperatures[0];
-        for (double temperature : temperatures) {
-            m = Math.max(temperature, m);
-        }
-        return m;
-    }
-
-    double findTempClosestToZero() {
-        return findTempClosestToValue(0.0);
-    }
-
-    double findTempClosestToValue(double tempValue) {
-        if (temperaturesNum == 0) {
-            throw new IllegalArgumentException();
-        }
-
-        double closestTemp = temperatures[0];
-        double currentClosest = Math.abs(temperatures[0] - tempValue);
-        for (int i = 0; i < temperaturesNum; i++) {
-            if (Math.abs(currentClosest
-                    - Math.abs(temperatures[i]
-                    - tempValue)) <= DELTA
-                    && temperatures[i] > tempValue) {
-                closestTemp = temperatures[i];
-            } else if (currentClosest > Math.abs(temperatures[i] - tempValue)) {
-                currentClosest = Math.abs(temperatures[i] - tempValue);
-                closestTemp = temperatures[i];
+    public double min() {
+        isEmpty();
+        double min = this.temperatureSeries[0];
+        for (int i = 0; i < this.length; i++) {
+            if (min > temperatureSeries[i]) {
+                min = temperatureSeries[i];
             }
         }
-        return closestTemp;
+        return min;
     }
-
-
-    private double[] findTempsWithCondition(boolean greater, double tempValue) {
-        TemperatureSeriesAnalysis tempsLess = new TemperatureSeriesAnalysis();
-        for (int i = 0; i < temperaturesNum; i++) {
-            if (temperatures[i] > tempValue
-                    && greater
-                    || temperatures[i] < tempValue
-                    && !greater) {
-                tempsLess.addTemps(temperatures[i]);
+    public double max() {
+        isEmpty();
+        double max = this.temperatureSeries[0];
+        for (int i = 0; i < this.length; i++) {
+            if (max < temperatureSeries[i]) {
+                max = temperatureSeries[i];
             }
         }
-        double[] res = new double[tempsLess.temperaturesNum];
-        System.arraycopy(tempsLess.temperatures, 0, res, 0, res.length);
-        return res;
+        return max;
+    }
+    public double findTempClosestToZero(){
+        return findTempClosestToValue(0);
     }
 
-    double[] findTempsLessThan(double tempValue) {
-        return findTempsWithCondition(false, tempValue);
-    }
-
-    double[] findTempsGreaterThan(double tempValue) {
-        return findTempsWithCondition(true, tempValue);
-    }
-
-    private void addOneTemp(double temp) {
-        if (capacity == temperaturesNum) {
-            double[] newArr = new double[capacity * 2];
-            System.arraycopy(temperatures, 0, newArr, 0, temperaturesNum);
-            temperatures = newArr;
-            capacity *= 2;
+    public double findTempClosestToValue(double tempValue) {
+        isEmpty();
+        double closestToVal = this.temperatureSeries[0];
+        for (int i = 0; i < this.length; i++) {
+            if (Math.abs(temperatureSeries[i] - tempValue) < Math.abs(closestToVal - tempValue)) {
+                closestToVal = this.temperatureSeries[i];
+            }
         }
-        temperatures[temperaturesNum] = temp;
-        temperaturesNum++;
+        return closestToVal;
     }
 
-    TempSummaryStatistics summaryStatistics() {
+    public double[] findTempsLessThen(double tempValue) {
+        double[] tempsLessThen = new double[this.length + 1];
+        int j = 0;
+        for (int i = 0; i < this.length; i++) {
+            if (this.temperatureSeries[i] < tempValue) {
+                tempsLessThen[j] = this.temperatureSeries[i];
+                j++;
+            }
+        }
+        return tempsLessThen;
+
+    }
+
+    public double[] findTempsGreaterThen(double tempValue) {
+        double[] tempsGreaterThen = new double[this.length + 1];
+        int j = 0;
+        for (int i = 0; i < this.length; i++) {
+            if (this.temperatureSeries[i] > tempValue) {
+                tempsGreaterThen[j] = this.temperatureSeries[i];
+                j++;
+            }
+        }
+        return tempsGreaterThen;
+    }
+
+    public TempSummaryStatistics summaryStatistics() {
+        isEmpty();
         return new TempSummaryStatistics(average(), deviation(), min(), max());
     }
 
-
-    int addTemps(double... temps) {
-        for (double temp : temps) {
-            if (temp < MIN_TEMP) {
-                throw new InputMismatchException();
+    public int addTemps(double... temps) {
+        if (capacity - length < temps.length) {
+            double[] newSeries = new double[this.length * 2];
+            double[] oldSeries = this.temperatureSeries;
+            if (this.length >= 0) {
+                System.arraycopy(oldSeries, 0, newSeries, 0, this.length);
             }
+            this.length = this.temperatureSeries.length + temps.length;
+            this.temperatureSeries = newSeries;
         }
-        for (double temp : temps) {
-            addOneTemp(temp);
+        int j = 0;
+        for (int i = this.length; i < this.length + temps.length; i++) {
+            this.temperatureSeries[i] = temps[j];
+            j += 1;
         }
-        return temperaturesNum;
+        this.capacity = this.temperatureSeries.length;
+        this.length+=temps.length;
+        return this.length;
     }
 }
